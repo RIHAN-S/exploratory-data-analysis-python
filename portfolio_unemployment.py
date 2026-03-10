69a729899f38063f0b5e6939c7a3082a51fd6e5a
RIHAN S <157547321+RIHAN-S@users.noreply.github.com>
2026-03-10 22:11:41 +0530

    Add files via upload

---
+++ portfolio_unemployment.py
@@ -0,0 +1,78 @@
+# Import pandas
+import pandas as pd
+
+# Import the required visualization libraries
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#read the csv file
+unemployment = pd.read_csv('clean_unemployment.csv')
+
+# Print the first five rows of unemployment
+print(unemployment.head())
+
+# Count the values associated with each continent in unemployment
+print(unemployment.value_counts('continent'))
+
+# Create a histogram of 2021 unemployment; show a full percent in each bin
+sns.histplot(data=unemployment, x='2021',binwidth=1)
+plt.show()
+
+# Update the data type of the 2019 column to a float
+unemployment["2019"] = unemployment["2019"].astype(float)
+# Print the dtypes to check your work
+print(unemployment.dtypes)
+
+# Define a Series describing whether each continent is outside of Oceania
+not_oceania = ~unemployment["continent"].isin(["Oceania"])
+
+# Print the minimum and maximum unemployment rates during 2021
+print(unemployment["2021"].min(), unemployment["2021"].max())
+
+# Create a boxplot of 2021 unemployment rates, broken down by continent
+sns.boxplot(data=unemployment,x="2021",y="continent")
+plt.show()
+
+# Print the mean and standard deviation of rates for 2019 and 2020 
+print(unemployment[["2019", "2020"]].agg(['mean','std']))
+
+# Print mean and standard deviation grouped by continent
+print(unemployment[["continent", "2019", "2020"]].groupby("continent").agg(["mean", "std"]))
+
+# Create continent_summary
+continent_summary = unemployment.groupby("continent").agg(
+    # Create the mean_rate_2021 column
+    mean_rate_2021=("2021",'mean'),
+    # Create the std_rate_2021 column
+    std_rate_2021=("2021",'std')
+)
+print(continent_summary)
+
+# Create a bar plot of continents and their average unemployment
+sns.barplot(data=unemployment,x="continent",y="2021")
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
